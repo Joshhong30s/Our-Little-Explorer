df0d33c1fe90e5dba9e8990c3d5fb59338d6bd09
JJ-practice <fixymefixyou@gmail.com>
2023-04-11 21:03:06 +0800

    04110

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -32,7 +32,7 @@ export default function Navbar() {
                   <Link href='/login'>
                     <Image
                       className='rounded-full border-2 border-yellow-400  drop-shadow-xl mx-auto '
-                      src='/cat.jpg'
+                      src='/baby.jpg'
                       alt='cat.jpg'
                       width={70}
                       height={70}
