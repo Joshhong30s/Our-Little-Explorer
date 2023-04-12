0f97c7b90507ff7e12c236d8e3a1baf5ff750684
Josh <fixymefixyou@gmail.com>
2023-04-12 18:26:09 +0800

    0412v6

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import link from 'next-link'
+import Link from 'next/link'
 import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import { useGetUserID } from '../hooks/useGetUserId'
