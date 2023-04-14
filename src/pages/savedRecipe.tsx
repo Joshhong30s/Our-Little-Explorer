169e5f96bb52baae9de0ba5b0d28666a72d6811e
Josh <fixymefixyou@gmail.com>
2023-04-14 14:40:31 +0800

    0414v6

--- src/pages/savedRecipe.tsx
+++ src/pages/savedRecipe.tsx
@@ -3,11 +3,16 @@ import Image from 'next/image'
 import { useState, useEffect } from 'react'
 import { useGetUserID } from '../hooks/useGetUserId'
 import { FaBaby } from 'react-icons/fa'
+import { useCookies } from 'react-cookie'
+import Link from 'next/link'
+import { RiHeartAddLine, RiHeartFill } from 'react-icons/ri'
 
 export default function SavedRecipes() {
   const [savedRecipes, setSavedRecipes] = useState([])
-
+  const [recipes, setRecipes] = useState([])
+  const [cookies, _] = useCookies(['access_token'])
   const userID = useGetUserID()
+
   useEffect(() => {
     const fetchSavedRecipes = async () => {
       try {
@@ -22,6 +27,50 @@ export default function SavedRecipes() {
 
     fetchSavedRecipes()
   }, [userID])
+
+  const saveRecipe = async (recipeID: string) => {
+    try {
+      const response = await axios.put(
+        'https://zero6babyserver.onrender.com/recipes',
+        {
+          recipeID,
+          userID,
+        },
+        { headers: { authorization: cookies.access_token } }
+      )
+      setSavedRecipes(response.data.savedRecipes)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const unsaveRecipe = async (recipeID: string) => {
+    try {
+      const response = await axios.delete(
+        'https://zero6babyserver.onrender.com/recipes',
+        {
+          data: {
+            recipeID,
+            userID,
+          },
+          headers: { authorization: cookies.access_token },
+        }
+      )
+      setSavedRecipes(response.data.savedRecipes)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const isRecipeSaved = (recipeID: string) => savedRecipes.includes(recipeID)
+
+  const toggleSaveRecipe = (recipeID: string) => {
+    if (isRecipeSaved(recipeID)) {
+      unsaveRecipe(recipeID)
+    } else {
+      saveRecipe(recipeID)
+    }
+  }
 
   return (
     <main>
@@ -41,12 +90,35 @@ export default function SavedRecipes() {
                 key={recipe._id}
               >
                 <div className='relative w-full h-96 sm:h-[450px] lg:h-[600px]'>
-                  <Image
-                    src={recipe.imageUrl}
-                    alt={recipe.name}
-                    className='object-cover'
-                    fill
-                  />
+                  <Link
+                    href={recipe.imageUrl}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <Image
+                      src={recipe.imageUrl}
+                      alt={recipe.name}
+                      className='object-cover'
+                      placeholder='blur'
+                      fill
+                    />
+                  </Link>
+                  <button
+                    className='absolute top-2 right-2 bg-neutral-50 bg-opacity-30 text-red-500 rounded-full p-3'
+                    onClick={() => toggleSaveRecipe(recipe._id)}
+                  >
+                    {isRecipeSaved(recipe._id) ? (
+                      <RiHeartFill
+                        size={40}
+                        className='text-red-500 text-base hover:scale-125'
+                      />
+                    ) : (
+                      <RiHeartAddLine
+                        size={40}
+                        className='text-base hover:scale-125'
+                      />
+                    )}
+                  </button>
                 </div>
                 <div className='p-4'>
                   <div className='flex justify-between items-center mb-4'>
