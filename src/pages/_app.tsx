03a3094af8eb17155401313af9734a1ded86a3a2
Josh <fixymefixyou@gmail.com>
2023-04-16 10:31:13 +0800

    appletouchicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { useCookies } from 'react-cookie'
 import dynamic from 'next/dynamic'
 import NoSSR from '@/components/NoSSR'
 import { Klee_One } from 'next/font/google'
+import Head from 'next/head'
 
 const klee = Klee_One({
   weight: '600',
@@ -15,6 +16,10 @@ const klee = Klee_One({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={klee.className}>
+      <Head>
+        <link rel='apple-touch-icon' href='/family.svg' />
+        <title>小寶成長紀錄</title>
+      </Head>
       <NoSSR>
         <Navbar />
         <Component {...pageProps} />
