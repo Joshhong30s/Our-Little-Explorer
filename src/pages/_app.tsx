3f0802c199f231404541ed2df06b52319223f17f
Josh <fixymefixyou@gmail.com>
2023-04-30 09:31:14 +0800

    v12

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Navbar from '../components/navbar'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import NoSSR from '@/components/NoSSR'
 import { Klee_One } from 'next/font/google'
 import Head from 'next/head'
 import Footer from '@/components/Footer'
@@ -17,9 +18,11 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel='apple-touch-icon' href='/family.jpg' />
         <title>小寶成長紀錄</title>
       </Head>
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
+      <NoSSR>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </NoSSR>
     </div>
   )
 }
