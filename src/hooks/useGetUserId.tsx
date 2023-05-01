59dee93f315f181a344371c01da7d6048cc0324d
Josh <fixymefixyou@gmail.com>
2023-05-02 06:37:42 +0800

    0502

--- src/hooks/useGetUserId.tsx
+++ src/hooks/useGetUserId.tsx
@@ -1,18 +1,18 @@
-export const useGetUserID = () => {
-  return window.localStorage.getItem('userID')
-}
-
-// 'use client'
-
-// import { useState, useEffect } from 'react'
-
 // export const useGetUserID = () => {
-//   const [userID, setUserID] = useState<string | null>(null)
-
-//   useEffect(() => {
-//     const id = window.localStorage.getItem('userID')
-//     setUserID(id)
-//   }, [])
-
-//   return userID
+//   return window.localStorage.getItem('userID')
 // }
+
+'use client'
+
+import { useState, useEffect } from 'react'
+
+export const useGetUserID = () => {
+  const [userID, setUserID] = useState<string | null>(null)
+
+  useEffect(() => {
+    const id = window.localStorage.getItem('userID')
+    setUserID(id)
+  }, [])
+
+  return userID
+}
