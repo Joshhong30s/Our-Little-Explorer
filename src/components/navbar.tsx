9c0750c7c6f59fa21218cb2bacdb74d8daec623f
Josh <fixymefixyou@gmail.com>
2023-05-02 08:27:06 +0800

    rename nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,28 @@
 'use client'
 import Link from 'next/link'
-import { IoMdMenu, IoMdClose, IoMdCloseCircle } from 'react-icons/io'
+import Image from 'next/image'
+
 import { useState } from 'react'
 import { useCookies } from 'react-cookie'
+
+import { IoMdMenu, IoMdClose, IoMdCloseCircle } from 'react-icons/io'
 import { TbDeviceAnalytics } from 'react-icons/tb'
-import Image from 'next/image'
 
 export default function Navbar() {
   const [cookies, setCookies] = useCookies(['access_token'])
-  const [navbar, setNavbar] = useState(false)
-  const [databar, setDatabar] = useState(false)
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false)
+  const [isDatabarOpen, setIsDatabarOpen] = useState(false)
   const logout = () => {
     setCookies('access_token', '')
     window.localStorage.clear()
   }
 
   return (
-    // outside flex div
     <nav className='bg-white mx-auto text-xl text-black px-6 py-4 items-center'>
       <div className='flex flex-col md:flex-row justify-center md:items-center md:justify-between'>
-        {/* navbar for md */}
         <div
           className={`md:flex md:pb-0 mt-2 md:my-2 basis-1/3 order-2 md:order-1 ${
-            navbar ? 'block' : 'hidden'
+            isNavbarOpen ? 'block' : 'hidden'
           }`}
         >
           <div className='xl:text-xl'>
@@ -55,27 +55,32 @@ export default function Navbar() {
           </div>
         </div>
 
-        {/* navbar */}
         <div className=' order-1 md:order-2 flex justify-between items-center '>
-          <button className='md:hidden' onClick={() => setNavbar(!navbar)}>
-            {navbar ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
+          <button
+            className='md:hidden'
+            aria-label='Toggle left'
+            onClick={() => setIsNavbarOpen(!isNavbarOpen)}
+          >
+            {isNavbarOpen ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
           </button>
-          {/* logo */}
 
           <Link href='/'>
             <Image
               className='rounded-full drop-shadow-xl mx-auto '
               src='/baby.jpg'
-              alt='cat.jpg'
+              alt='baby'
               width={70}
               height={70}
               priority={true}
             />
           </Link>
 
-          {/* monitor button for mobile */}
-          <button className='md:hidden' onClick={() => setDatabar(!databar)}>
-            {databar ? (
+          <button
+            className='md:hidden'
+            aria-label='Toggle right'
+            onClick={() => setIsDatabarOpen(!isDatabarOpen)}
+          >
+            {isDatabarOpen ? (
               <IoMdCloseCircle size={30} />
             ) : (
               <TbDeviceAnalytics size={30} />
@@ -83,12 +88,10 @@ export default function Navbar() {
           </button>
         </div>
 
-        {/* databar for mobile */}
         <div
-          className={`order-3 justify-end md:flex md:justify-end md:space-x-10 py-2 md:text-lg lg:text-xl basis-1/3 ${
-            databar ? 'block' : 'hidden'
+          className={`order-3 justify-end md:flex md:justify-end md:space-x-10 py-2 md:text-lg lg:text-xl basis-1/3 text-right ${
+            isDatabarOpen ? 'block' : 'hidden'
           }`}
-          style={{ textAlign: 'right' }}
         >
           <div className='mb-4 md:mb-0 md:ml-6 hover:bg-teal-980 hover:text-gray-50 hover:p-4 hover:rounded-md'>
             <Link href='/dashboard'>成長圖表</Link>
