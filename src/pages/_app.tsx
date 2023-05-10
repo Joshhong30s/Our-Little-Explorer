774e2aa83868e12483b82c4ea3be47cf9bf7bb21
JJ-practice <fixymefixyou@gmail.com>
2023-05-10 17:30:15 +0800

    0510

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,18 @@ import NoSSR from '@/components/NoSSR'
 import { Klee_One } from 'next/font/google'
 import Head from 'next/head'
 import Footer from '@/components/Footer'
+import localFont from 'next/font/local'
 
 const klee = Klee_One({
   weight: '600',
   subsets: ['latin'],
 })
 
+const wenkai = localFont({ src: './LXGWWenKaiTC-Regular.ttf' })
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={klee.className}>
+    <div className={wenkai.className}>
       <Head>
         <link rel='apple-touch-icon' href='/family.jpg' />
         <title>小寶成長紀錄</title>
