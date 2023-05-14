70872c044c8b22b468964aa040d9a45b39434a77
JJ-practice <fixymefixyou@gmail.com>
2023-05-14 08:50:05 +0800

    v21

--- src/pages/message.tsx
+++ src/pages/message.tsx
@@ -132,10 +132,10 @@ export default function Message() {
 
   return (
     <div
-      className='mx-auto p-4 min-h-screen  bg-cover bg-center '
+      className='mx-auto p-8 min-h-screen  bg-cover bg-center '
       style={{ backgroundImage: "url('bao12.jpeg')" }}
     >
-      <div className='flex flex-col md:flex-row md:justify-around gap-8 justify-center'>
+      <div className='flex flex-col md:flex-row gap-8 justify-center items-center'>
         <div className='md:w-1/3 bg-white rounded-md shadow-md p-6'>
           <h2 className='text-2xl text-center font-medium mb-6'>
             我要留言給小寶
