f21ccff9cc3bf5449fb4ead471bfa506dcb073cd
JJ-practice <fixymefixyou@gmail.com>
2023-05-15 14:44:01 +0800

    b67

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false)
-    }, 2000) // or however long you think it might take to load the images
+    }, 1000) // or however long you think it might take to load the images
 
     return () => clearTimeout(timer) // cleanup on unmount
   }, [])
